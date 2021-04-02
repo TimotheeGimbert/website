5d66787525afa0e15d47bcb3399ba47a34054568
TimotheeGimbert <timothee.gimbert@free.fr>
2021-04-02 17:31:21 +0200

    PAGE INDEX V2 terminée

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-// On va tracker la position de la souris dans la "zone"
- const zone = document.querySelector("#zone");
+// Mouse tracking within #zone
+const zone = document.querySelector("#zone");
 zone.addEventListener("mousemove", function(event) {
     const xPos = event.offsetX;
-
     const hslText = "hsl(" + xPos + ", 10%, 50%)";
     zone.style.backgroundColor = hslText;
 });
+
 const villes = ["bordeaux", "paris", "royan", "nantes"];
 for (let ville in villes) {
     var askWeather = new XMLHttpRequest();
